2372b5bd9a29886ba00a53b639e312b23a045019
Ghalai <144619930+Ghalai@users.noreply.github.com>
2023-12-02 16:56:26 +0300

    M

--- LinkedListADT.java
+++ LinkedListADT.java
@@ -1,36 +1,6 @@
 
 public class LinkedListADT<T extends Comparable<T>> {
-    class Node<T>{
-        public T data;
-        public Node<T> next;
-        public Node () {
-            data = null;
-            next = null;
-        }
-        public Node (T val) {
-            data = val;
-            next = null;
-        }
-        
-
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
-            this.data = data;
-        }
-
-        public Node<T> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<T> next) {
-            this.next = next;
-        }
-
-    }    
-
+    
 
     private Node<T> head;
     private Node<T> current;
